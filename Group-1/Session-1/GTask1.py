e9a678c445996d02b81721aa36b456c2c04c6730
Mat-84 <matinnor084@gmail.com>
2025-03-11 12:15:34 +0330

    update group task

--- Group-1/Session-1/GTask1.py
+++ Group-1/Session-1/GTask1.py
@@ -34,9 +34,22 @@ def AgeInpt():
     #baraye gereftan vrodi sen hast
     print("")
 
-def EmailInpt():
+def EmailInpt(email:str):
     #baraye gereftan vrodi email hast
-    print("")
+    if len(email) <= 4 :
+        return "email bayad bishtar az 4 har bashad"
+    elif not email.isalnum() :
+        return "email bayad tarkibi az horof va adad bashad"
+    elif not email[0].isalpha() :
+        return "harf aval email bayad bozorg bashad"
 
-CClear()
-NameInpt()
+    elif email in ["@",".","-","_"] :
+        return "namotabar ast"
+    return f"{email}@gmail.com"
+user = input("email ra vared konid : ")
+result = EmailInpt(user)
+print(result)
+
+
+#CClear()
+#NameInpt()
