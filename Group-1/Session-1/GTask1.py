9fec8d9efec811637c06b4c2007fe1a0c980e95e
Mat-84 <matinnor084@gmail.com>
2025-03-13 14:39:31 +0330

    update Gtask1 email input

--- Group-1/Session-1/GTask1.py
+++ Group-1/Session-1/GTask1.py
@@ -36,20 +36,63 @@ def AgeInpt():
 
 def EmailInpt():
     #baraye gereftan vrodi email hast
-    print("")
+    global email
+    uinput = input("email ra vared konid  \n\n\n\n\n^__^ ")
+    if not uinput :
+        CClear()
+        print("lotfan email ra vared konid\n\n\n")
+        EmailInpt()
+    if len(uinput) <= 4 :
+        CClear()
+        print("email bayad bishtaraz 4 harf basheh\n\n\n")
+        EmailInpt()
+    elif uinput[0].isdigit():
+        CClear()
+        print("email mbayad ba adad shoro shavad\n\n\n")
+        EmailInpt()
+    elif not uinput.endswith("@gmail.com") :
+        CClear()
+        print("bayad @gmail.com ra vared konid\n\n\n")
+        EmailInpt()
+    elif uinput.count("@") != 1 :
+        CClear()
+        print("khata faghat yek @ misheh vared kard\n\n\n")
+        EmailInpt()
+    elif not any(harf.isalpha() for harf in uinput):
+        CClear()
+        print("lotfan harf vared konid\n\n\n")
+        EmailInpt()
+    elif any(char in['!','#','$','%','^','&','*',',','-','+','?','_','=','<>','/'] for char in uinput):
+        CClear()
+        print("dar email az character hay vijeh nmisheh estefade kard\n\n\n")
+        EmailInpt()
+    else :
+        CClear()
+        email = uinput
+        print(f"email shoma : {email}")
+        EmailInpt()
+    # gmail.org453d@gmail.com
+    # 23432.e.42@gmail.com
 
-form = f"""
+    
 
-name  : {name}
 
-age   :
-
-email :
-
-       """
 
 #CClear()
 #NameInpt()
+#CClear
 #AgeInpt()
-#EmailInpt()
+CClear()
+EmailInpt()
+
+form = f"""
+
+name  : 
+
+age   :
+
+email : 
+
+       """
+#CClear()
 #print(form)
