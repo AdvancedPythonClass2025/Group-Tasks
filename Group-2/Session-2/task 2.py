e853b62d9710f85c548f7485ef8648796d4c67fb
hoda <ahmadihoda058@gmail.com>
2025-04-19 15:15:02 +0330

    Update task 2.py

--- Group-2/Session-2/task 2.py
+++ Group-2/Session-2/task 2.py
@@ -8,3 +8,11 @@
 file.write(f"{datetime.datetime.now()} - تابع '{func.__name__}' اجرا شد\n")
             return result
         return wrapper
+
+
+  def search_by_author(self, author):
+         result = []
+        for book in self.books:
+          if author.lower() in book['author'].lower():
+                 results.append(book)
+            return results
