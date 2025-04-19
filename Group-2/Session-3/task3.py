c566a528a61a5ed59d3a14a2a3d07a77ee6a9e1d
hoda <ahmadihoda058@gmail.com>
2025-04-19 15:35:26 +0330

    Update task3.py

--- Group-2/Session-3/task3.py
+++ Group-2/Session-3/task3.py
@@ -1,3 +1,7 @@
 class Manager(Employee):
     def manage_team(self):
         print(f"{self.name} در حال مدیریت تیم است.")
+        
+class developer(Employee):
+     def write_code(self):
+         print(f"{self.name} در حال نوشتن کد است.")
